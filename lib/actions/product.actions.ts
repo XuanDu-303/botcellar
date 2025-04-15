b114efe8922c050b09642016de91d8b365ecf91a
XuanDu-303 <phamxuandu13102003@gmail.com>
2025-04-15 22:53:13 +0700

    Create product detail page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,24 +1,25 @@
-'use server'
+"use server";
 
-import { connectToDatabase } from '@/lib/db'
-import Product, { IProduct } from '@/lib/db/models/product.model'
+import { connectToDatabase } from "@/lib/db";
+import Product, { IProduct } from "@/lib/db/models/product.model";
+import { PAGE_SIZE } from "../constants";
 
 export async function getAllCategories() {
-  await connectToDatabase()
+  await connectToDatabase();
   const categories = await Product.find({ isPublished: true }).distinct(
-    'category'
-  )
-  return categories
+    "category"
+  );
+  return categories;
 }
 
 export async function getProductsForCard({
   tag,
   limit = 4,
 }: {
-  tag: string
-  limit?: number
+  tag: string;
+  limit?: number;
 }) {
-  await connectToDatabase()
+  await connectToDatabase();
   const products = await Product.aggregate([
     { $match: { tags: { $in: [tag] }, isPublished: true } },
     { $sort: { createdAt: -1 } },
@@ -26,17 +27,17 @@ export async function getProductsForCard({
     {
       $project: {
         name: 1,
-        href: { $concat: ['/product/', '$slug'] },
-        image: { $arrayElemAt: ['$images', 0] },
+        href: { $concat: ["/product/", "$slug"] },
+        image: { $arrayElemAt: ["$images", 0] },
       },
     },
-  ])
-  
+  ]);
+
   return JSON.parse(JSON.stringify(products)) as {
-    name: string
-    href: string
-    image: string
-  }[]
+    name: string;
+    href: string;
+    image: string;
+  }[];
 }
 
 // GET PRODUCTS BY TAG
@@ -44,15 +45,51 @@ export async function getProductsByTag({
   tag,
   limit = 10,
 }: {
-  tag: string
-  limit?: number
+  tag: string;
+  limit?: number;
 }) {
-  await connectToDatabase()
+  await connectToDatabase();
   const products = await Product.find({
     tags: { $in: [tag] },
     isPublished: true,
   })
-    .sort({ createdAt: 'desc' })
-    .limit(limit)
-  return JSON.parse(JSON.stringify(products)) as IProduct[]
+    .sort({ createdAt: "desc" })
+    .limit(limit);
+  return JSON.parse(JSON.stringify(products)) as IProduct[];
+}
+
+export async function getProductBySlug(slug: string) {
+  await connectToDatabase();
+  const product = await Product.findOne({ slug, isPublished: true });
+  if (!product) throw new Error("Product not found");
+  return JSON.parse(JSON.stringify(product)) as IProduct;
+}
+
+export async function getRelatedProductsByCategory({
+  category,
+  productId,
+  limit = PAGE_SIZE,
+  page = 1,
+}: {
+  category: string;
+  productId: string;
+  limit?: number;
+  page: number;
+}) {
+  await connectToDatabase();
+  const skipAmount = (Number(page) - 1) * limit;
+  const conditions = {
+    isPublished: true,
+    category,
+    _id: { $ne: productId },
+  };
+  const products = await Product.find(conditions)
+    .sort({ numSales: "desc" })
+    .skip(skipAmount)
+    .limit(limit);
+  const productsCount = await Product.countDocuments(conditions);
+  return {
+    data: JSON.parse(JSON.stringify(products)) as IProduct[],
+    totalPages: Math.ceil(productsCount / limit),
+  };
 }
