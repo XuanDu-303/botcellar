218ed3ce04e8732aa45e74f31edb598f71bcb693
XuanDu-303 <phamxuandu13102003@gmail.com>
2025-04-23 02:23:30 +0700

    Pay Order By PayPal

---
+++ components/shared/order/order-details-form.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { IOrder } from "@/lib/db/models/order.model";
+import { cn, formatDateTime } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import ProductPrice from "../product/product-price";
+
+export default function OrderDetailsForm({
+  order,
+}: {
+  order: IOrder;
+  isAdmin: boolean;
+}) {
+  const {
+    shippingAddress,
+    items,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paymentMethod,
+    isPaid,
+    paidAt,
+    isDelivered,
+    deliveredAt,
+    expectedDeliveryDate,
+  } = order;
+
+  return (
+    <div className="grid md:grid-cols-3 md:gap-5">
+      <div className="overflow-x-auto md:col-span-2 space-y-4">
+        <Card>
+          <CardContent className="p-4 gap-4">
+            <h2 className="text-xl pb-4">Shipping Address</h2>
+            <p>
+              {shippingAddress.fullName} {shippingAddress.phone}
+            </p>
+            <p>
+              {shippingAddress.street}, {shippingAddress.city},{" "}
+              {shippingAddress.province}, {shippingAddress.postalCode},{" "}
+              {shippingAddress.country}{" "}
+            </p>
+
+            {isDelivered ? (
+              <Badge>
+                Delivered at {formatDateTime(deliveredAt!).dateTime}
+              </Badge>
+            ) : (
+              <div>
+                {" "}
+                <Badge variant="destructive">Not delivered</Badge>
+                <div>
+                  Expected delivery at{" "}
+                  {formatDateTime(expectedDeliveryDate!).dateTime}
+                </div>
+              </div>
+            )}
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className="p-4 gap-4">
+            <h2 className="text-xl pb-4">Payment Method</h2>
+            <p>{paymentMethod}</p>
+            {isPaid ? (
+              <Badge>Paid at {formatDateTime(paidAt!).dateTime}</Badge>
+            ) : (
+              <Badge variant="destructive">Not paid</Badge>
+            )}
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent className="p-4 gap-4">
+            <h2 className="text-xl pb-4">Order Items</h2>
+            <Table>
+              <TableHeader>
+                <TableRow className="bg-gray-100">
+                  <TableHead className="text-left">Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {items.map((item) => (
+                  <TableRow
+                    key={item.slug}
+                    className="hover:bg-gray-50 transition"
+                  >
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center gap-3"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={48}
+                          height={48}
+                          className="rounded-md border"
+                        />
+                        <div className="font-medium">{item.name}</div>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="text-center text-sm text-muted-foreground">
+                      {item.quantity}
+                    </TableCell>
+                    <TableCell className="text-right font-semibold text-green-700">
+                      ${item.price.toFixed(2)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </div>
+      <div>
+        <Card>
+          <CardContent className="p-4  space-y-4 gap-4">
+            <h2 className="text-xl pb-4">Order Summary</h2>
+            <div className="flex justify-between">
+              <div>Items</div>
+              <div>
+                {" "}
+                <ProductPrice price={itemsPrice} plain />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <div>Tax</div>
+              <div>
+                {" "}
+                <ProductPrice price={taxPrice} plain />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <div>Shipping</div>
+              <div>
+                {" "}
+                <ProductPrice price={shippingPrice} plain />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <div>Total</div>
+              <div>
+                {" "}
+                <ProductPrice price={totalPrice} plain />
+              </div>
+            </div>
+
+            {!isPaid && ["Stripe", "PayPal"].includes(paymentMethod) && (
+              <Link
+                className={cn(buttonVariants(), "w-full")}
+                href={`/checkout/${order._id}`}
+              >
+                Pay Order
+              </Link>
+            )}
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
